38ba83a7da11fb39cd0606598b1f2e6f945c8b61
Muriel Carvalho <muriel.teles@grupomateus.com.br>
2021-07-08 16:57:55 -0300

    Commit do projeto

---
+++ src/main/java/br/com/murielmagno/personapi/PersonapiApplication.java
@@ -0,0 +1,14 @@
+package br.com.murielmagno.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+		System.out.println("teste");
+	}
+
+}
