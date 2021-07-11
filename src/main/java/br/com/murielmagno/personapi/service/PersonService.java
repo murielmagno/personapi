1d47d72c59b262d9343f05011c113309e5fe2ad1
murielmagno <magnomuriel@gmail.com>
2021-07-11 18:28:13 -0300

    Subindo as operações de delete e put

--- src/main/java/br/com/murielmagno/personapi/service/PersonService.java
+++ src/main/java/br/com/murielmagno/personapi/service/PersonService.java
@@ -28,7 +28,7 @@ public class PersonService {
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO.builder().message("Pessoa criada com o ID " + savedPerson.getId()).build();
+        return createdMessageResponse(savedPerson.getId(), "Pessoa criada com o ID ");
     }
 
     public List<PersonDTO> listAll() {
@@ -38,8 +38,28 @@ public class PersonService {
     }
 
     public PersonDTO findByid(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExist(id);
         return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExist(id);
+        personRepository.deleteById(id);
+    }
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExist(id);
+        Person personToUpdate = personMapper.toModel(personDTO);
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createdMessageResponse(updatedPerson.getId(), "Pessoa atualizada com o ID ");
+    }
+
+    private Person verifyIfExist(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createdMessageResponse(Long id, String message) {
+        return MessageResponseDTO.builder().message(message + id).build();
     }
 }
